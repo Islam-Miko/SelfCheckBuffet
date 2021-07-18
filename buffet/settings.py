af17a77d3bc9d9e42202356f94d7d6693e304abc
Islam-Miko <islamimanhod@gmail.com>
2021-07-18 22:32:16 +0600

    allowed_host, django-heroku and settings,

--- buffet/settings.py
+++ buffet/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os.path
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-3(q#v0s)$3!4e6-xc9(8$_()wp7@gjk0vlckjd8_&3gh#%z#s0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://easyitselfcheck.herokuapp.com/']
 
 
 # Application definition
@@ -142,4 +143,6 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
 
